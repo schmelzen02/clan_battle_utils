66741c3c4f719ad1d5184fc1180ddf4d71d0fa61
schmelzen02 <schmelzen.v02@gmail.com>
2023-06-14 22:57:13 +0900

    残りのデータ取得処理を追加

--- aggregate.py
+++ aggregate.py
@@ -1,8 +1,11 @@
 import re
+import datetime
+import calendar
 
 # 4段階目アンケート結果の読み込み
 # 「{'1物': ['player1', 'player2'], '2魔': ['player1', 'player3'], ...}」形式のdictを返却します
 def import_questionnaire_phase4() -> dict:
+    print('4段階目アンケート結果を読み込んでいます...')
     result = {}
 
     with open('#4段階目アンケート結果.txt', 'r', encoding='utf-8') as f:
@@ -17,19 +20,34 @@ def import_questionnaire_phase4() -> dict:
                 result[key] = line.rstrip(' \r?\n').split(' ')
                 key = None
 
+    # debug
+    # print(result)
+
     return result
 
-# 初日アンケート結果の読み込み
-def import_questionnaire_first_day():
-    return {}
+# 初日4段階目参加メンバーを取得
+def import_questionnaire_first_day() -> list:
+    print('初日アンケート結果を読み込んでいます...')
+    result = []
 
-# 臨時対応状況の読み込み
-def import_temporary_route():
-    return {}
+    with open('#初日アンケート結果.txt', 'r', encoding='utf-8') as f:
+        is_phase4_line = False
+        for line in f:
+            # 初日アンケートの「5. 4段階目」「6. 朝活貫通」を初日4段階目参加メンバーとしてカウントする
+            key_line = re.match('^[56]\..+$', line)
+            if key_line:
+                is_phase4_line = True
+            elif is_phase4_line:
+                # 4段階目タイトル行の直後の行を4段階目参加メンバーとしてカウント
+                result += line.rstrip(' \r?\n').split(' ')
+                is_phase4_line = False
+
+    return list(set(result))
 
 # 本日の凸状況の読み込み
 # 「{'player1': ['1物', '2魔', '5物'], ...}」形式のdictを返却します
 def import_today_route() -> dict:
+    print('本日の凸状況を読み込んでいます...')
     result = {}
 
     with open('#本日の凸状況.txt', 'r', encoding='utf-8') as f:
@@ -44,38 +62,48 @@ def import_today_route() -> dict:
                 route3 = route_line.group(4)[0:2]
                 result[key] = [route1, route2, route3]
 
+    # debug
+    # print(result)
+
     return result
 
 # 現在の周回数の読み込み
-def import_current_lap():
+def import_current_lap() -> list:
+    print('現在の周回数を読み込んでいます...')
+
     with open('#現在の周回数.txt', 'r', encoding='utf-8') as f:
         return [line.rstrip('\r|\n') for line in f.readlines()]
-        
-print('4段階目アンケート結果を読み込んでいます...')
+
+# 現在のクラバト日付（1-5日目）を取得します
+def get_current_day() -> int:
+    # 5:00を基準とした現在日付を取得します
+    # たとえば「6/27 4:50」は6/26として扱われます
+    now = (datetime.datetime.now() - datetime.timedelta(hours=5))
+    # today = now.day
+    # ★debug
+    today = 27
+    _, lastday = calendar.monthrange(now.year, now.month)
+
+    result = today + 6 - lastday
+
+    # debug
+    # print(result)
+
+    return result
+
+# 凸先変更情報の読み込み
+def import_route_change_info():
+    print('凸先変更情報を読み込んでいます...')
+    return {}
+
 questionnaire_phase4 = import_questionnaire_phase4()
-
-print('初日アンケート結果を読み込んでいます...')
 questionnaire_first_day = import_questionnaire_first_day()
-
-print('臨時対応状況を読み込んでいます...')
-temporary_route = import_temporary_route()
-
-print('本日の凸状況を読み込んでいます...')
+temporary_route = import_route_change_info()
 today_route = import_today_route()
-
-print('現在の周回数を読み込んでいます...')
 current_lap = import_current_lap()
+current_day = get_current_day()
 
 # 以下デバッグ
-print()
-print('本日の凸状況')
-# print(today_route)
 
-print()
-print('4段階目アンケート結果')
-print(questionnaire_phase4)
 
-print()
-print('現在の周回数')
-# print(current_lap)
 
